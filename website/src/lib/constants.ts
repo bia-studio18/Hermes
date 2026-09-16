b7b533edb0a4720d206cd11478928b7a45d96185
ryomenhaider <onlinep680@gmail.com>
2026-09-16 17:54:21 +0500

    fixed the url redirections

--- website/src/lib/constants.ts
+++ website/src/lib/constants.ts
@@ -5,4 +5,4 @@ export const GITHUB_URL = "https://github.com/ryomenhaider/Hermes";
 export const DISCORD_URL = "https://discord.gg/UeJuEz4YdS";
 export const X_URL = "https://x.com/";
 export const LINKEDIN_URL = "https://www.linkedin.com/company/hermes-plt";
-export const DOCS_URL = "docs.hermes-plt.xyz";
+export const DOCS_URL = "https://docs.hermes-plt.xyz";
